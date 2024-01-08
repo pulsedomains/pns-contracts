4f2324659b80c7bac2faad6639b701bd7c18259a
Sinh Vu <khacsinh.vu.16091994@gmail.com>
2024-01-08 22:02:02 +0700

    chore: update TransferOwnership task

--- deploy/utils/02_transfer_ownership.ts
+++ deploy/utils/02_transfer_ownership.ts
@@ -6,18 +6,56 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts } = hre
   const { owner } = await getNamedAccounts()
 
-  const controller = await ethers.getContract('ETHRegistrarController', owner)
-  let tx = await controller.transferOwnership(
+  const root = await ethers.getContract('Root', owner)
+  let tx = await root.transferOwnership(
     '0xBcf8996B20E92304206cE8643E273Fef5E22f6Ef',
   )
-  console.log(`Transferring owner (tx: ${tx.hash})`)
+  console.log(`Transferring Root owner (tx: ${tx.hash})`)
+  await tx.wait()
+
+  const baseRegistrarImpl = await ethers.getContract(
+    'BaseRegistrarImplementation',
+    owner,
+  )
+  tx = await baseRegistrarImpl.transferOwnership(
+    '0xBcf8996B20E92304206cE8643E273Fef5E22f6Ef',
+  )
+  console.log(`Transferring BaseRegistrarImplementation owner (tx: ${tx.hash})`)
+  await tx.wait()
+
+  const reserveRegistrar = await ethers.getContract('ReverseRegistrar', owner)
+  tx = await reserveRegistrar.transferOwnership(
+    '0xBcf8996B20E92304206cE8643E273Fef5E22f6Ef',
+  )
+  console.log(`Transferring ReverseRegistrar owner (tx: ${tx.hash})`)
+  await tx.wait()
+
+  const universalResolver = await ethers.getContract('UniversalResolver', owner)
+  tx = await universalResolver.transferOwnership(
+    '0xBcf8996B20E92304206cE8643E273Fef5E22f6Ef',
+  )
+  console.log(`Transferring UniversalResolver owner (tx: ${tx.hash})`)
+  await tx.wait()
+
+  const ownedResolver = await ethers.getContract('OwnedResolver', owner)
+  tx = await ownedResolver.transferOwnership(
+    '0xBcf8996B20E92304206cE8643E273Fef5E22f6Ef',
+  )
+  console.log(`Transferring OwnedResolver owner (tx: ${tx.hash})`)
   await tx.wait()
 
   const nameWrapper = await ethers.getContract('NameWrapper', owner)
   tx = await nameWrapper.transferOwnership(
     '0xBcf8996B20E92304206cE8643E273Fef5E22f6Ef',
   )
-  console.log(`Transferring owner (tx: ${tx.hash})`)
+  console.log(`Transferring NameWrapper owner (tx: ${tx.hash})`)
+  await tx.wait()
+
+  const controller = await ethers.getContract('ETHRegistrarController', owner)
+  tx = await controller.transferOwnership(
+    '0xBcf8996B20E92304206cE8643E273Fef5E22f6Ef',
+  )
+  console.log(`Transferring ETHRegistrarController owner (tx: ${tx.hash})`)
   await tx.wait()
 }
 
